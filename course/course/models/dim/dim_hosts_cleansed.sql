4b2f8b838948226b424d0f45453acc4a9201fb36
Brian Strubel <brian.strubel@gmail.com>
2025-04-21 13:33:18 -0400

    create hosts dim table

---
+++ course/course/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+WITH src_hosts AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_hosts') }}
+)
+SELECT
+    HOST_ID,
+    NVL(HOST_NAME, 'Anonymous') AS host_name,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM
+    src_hosts
